260a19504c2bad7716149176a4857197ef92f06b
杨丹莹 <hzydy@tairanchina.com>
2019-02-19 14:05:19 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const webpack = require('webpack')
 const HtmlPlugin = require('html-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const  isDev = process.env.NODE_ENV === 'development'
 
@@ -9,10 +10,10 @@ const config = {
   target: 'web',
   mode: isDev ? 'development' : 'production',
   entry: path.join(__dirname, 'src/index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
+  // output: {
+  //   path: path.join(__dirname, 'dist'),
+  //   filename: 'bundle.[hash:8].js'
+  // },
   plugins: [
     new VueLoaderPlugin(),
     new webpack.DefinePlugin({
@@ -37,34 +38,6 @@ const config = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(styl|stylus)$/,
-        use: [
-          'style-loader', 
-          'css-loader', 
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', 
-          'css-loader', 
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          'sass-loader'
-        ]
-      },
-      {
         test: /\.(png|gif|jpe?g|svg)$/,
         use: {
           loader: 'url-loader',
@@ -79,6 +52,21 @@ const config = {
 }
 
 if (isDev) {
+  console.warn('dev')
+  config.module.rules.push({
+    test: /\.(styl|stylus)$/,
+    use: [
+      'style-loader', 
+      'css-loader', 
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     host: '0.0.0.0',
@@ -90,6 +78,48 @@ if (isDev) {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   )
+} else {
+  console.warn('prod')
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output = {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[chunkhash:8].js'
+  }
+  config.module.rules.push(      {
+    test: /\.(styl|stylus)$/,
+    use: ExtractPlugin.extract({
+      fallback: 'style-loader', 
+      use: [
+        'css-loader', 
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true
+          }
+        },
+        'stylus-loader'
+      ]
+    })
+  })
+  config.plugins.push(
+    new ExtractPlugin('styles.[hash:8].css')
+    // new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
+    // new webpack.optimize.CommonsChunkPlugin({name: 'runtime'})
+  )
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+            name: "commons",
+            chunks: "initial",
+            minChunks: 2
+        }
+      }
+    }
+  }
 }
 
 
